f182f4dcc800416224a4a4965560331ef8f77a5c
Rajat Verma <rajatv785v@gmail.com>
2023-03-03 15:23:45 +0530

    push

--- index.js
+++ index.js
@@ -17,145 +17,145 @@ app.use(cors( {
 app.use(express.json());
 
 //Sign in API
-// app.post('/signin',async(req,resp)=>{
-//   if(req.body.email && req.body.password){
-//     let user=await User.findOne(req.body).select('-password');
-//     if(user){
-//         return resp.send(user);
-//     }else{
-//         return resp.send({result:"failed"})
-//     }
-//   }else{
-//     return resp.send({result:'enter both field'})
-//   }
-// });
+app.post('/signin',async(req,resp)=>{
+  if(req.body.email && req.body.password){
+    let user=await User.findOne(req.body).select('-password');
+    if(user){
+        return resp.send(user);
+    }else{
+        return resp.send({result:"failed"})
+    }
+  }else{
+    return resp.send({result:'enter both field'})
+  }
+});
 
 // //Products API
 
-// app.post('/add-product',async(req,resp)=>{
-//     let product=new Products(req.body);
-//     let result=await product.save();
-//     return resp.status(200).json(result);
-// });
+app.post('/add-product',async(req,resp)=>{
+    let product=new Products(req.body);
+    let result=await product.save();
+    return resp.status(200).json(result);
+});
 
-// app.get('/products',async(req,resp)=>{
-//     let result=await Products.find();
-//     return resp.status(200).json(result);
-//     // resp.send(result);
-// })
+app.get('/products',async(req,resp)=>{
+    let result=await Products.find();
+    return resp.status(200).json(result);
+    // resp.send(result);
+})
 
-// app.delete('/product/:id',async(req,resp)=>{
-//     let result=await Products.deleteOne({_id:req.params.id})
-//     return resp.status(200).json(result)
-// })
+app.delete('/product/:id',async(req,resp)=>{
+    let result=await Products.deleteOne({_id:req.params.id})
+    return resp.status(200).json(result)
+})
 
-// app.get('/product/:id',async(req,resp)=>{
-//     let result=await Products.findOne({_id:req.params.id})
-//     return resp.status(200).json(result)
-// })
+app.get('/product/:id',async(req,resp)=>{
+    let result=await Products.findOne({_id:req.params.id})
+    return resp.status(200).json(result)
+})
 
-// app.put('/product/:id',async(req,resp)=>{
-//     let result=await Products.updateOne(
-//         {_id:req.params.id},
-//         {
-//           $set:req.body
-//         }
-//     )
-//     return resp.status(200).json(result)
-// })
+app.put('/product/:id',async(req,resp)=>{
+    let result=await Products.updateOne(
+        {_id:req.params.id},
+        {
+          $set:req.body
+        }
+    )
+    return resp.status(200).json(result)
+})
 
 
-// //Sale API
+//Sale API
 
-// app.post('/add-sale',async(req,resp)=>{
-//     let sales=new Sales(req.body);
-//     let result=await sales.save();
-//     return resp.status(200).json(result);
+app.post('/add-sale',async(req,resp)=>{
+    let sales=new Sales(req.body);
+    let result=await sales.save();
+    return resp.status(200).json(result);
 
-// })
+})
 
-// app.get('/sale',async(req,resp)=>{
-//     let result = await Sales.find();
-//     return resp.status(200).json(result);
-//     // resp.send(result)
+app.get('/sale',async(req,resp)=>{
+    let result = await Sales.find();
+    return resp.status(200).json(result);
+    // resp.send(result)
     
-// })
+})
 
-// app.delete('/sale/:id',async(req,resp)=>{
-//     let result=await Sales.deleteOne({_id:req.params.id})
-//     return resp.status(200).json(result)
-// })
+app.delete('/sale/:id',async(req,resp)=>{
+    let result=await Sales.deleteOne({_id:req.params.id})
+    return resp.status(200).json(result)
+})
 
-// app.get('/sale/:id',async(req,resp)=>{
-//     let result=await Sales.findOne({_id:req.params.id})
-//     return resp.status(200).json(result)
-// })
+app.get('/sale/:id',async(req,resp)=>{
+    let result=await Sales.findOne({_id:req.params.id})
+    return resp.status(200).json(result)
+})
 
-// app.put('/sale/:id',async(req,resp)=>{
-//     let result =await Sales.updateOne(
-//         {_id:req.params.id},
-//         {
-//             $set:req.body
-//         }
-//     )
-//     return resp.status(200).json(result)
-// })
-// //Purchase API
+app.put('/sale/:id',async(req,resp)=>{
+    let result =await Sales.updateOne(
+        {_id:req.params.id},
+        {
+            $set:req.body
+        }
+    )
+    return resp.status(200).json(result)
+})
+//Purchase API
 
-// app.post('/add-purchase',async(req,resp)=>{
-//     let purchase=new Purchases(req.body);
-//     let result=await purchase.save();
-//    return resp.status(200).json(result)
-// })
+app.post('/add-purchase',async(req,resp)=>{
+    let purchase=new Purchases(req.body);
+    let result=await purchase.save();
+   return resp.status(200).json(result)
+})
 
-// app.get('/purchase',async(req,resp)=>{
-//     let result=await Purchases.find();
-//     return resp.status(200).json(result);
-// })
+app.get('/purchase',async(req,resp)=>{
+    let result=await Purchases.find();
+    return resp.status(200).json(result);
+})
 
-// //Returns API
+//Returns API
 
-// app.post('/add-return',async(req,resp)=>{
-//     let returns = new Returns(req.body);
-//     let result =await returns.save();
-//     const endata={
-//         sucess:true,
-//         data:returns
-//     }
-//     return resp.status(200).json(endata);
-// })
+app.post('/add-return',async(req,resp)=>{
+    let returns = new Returns(req.body);
+    let result =await returns.save();
+    const endata={
+        sucess:true,
+        data:returns
+    }
+    return resp.status(200).json(endata);
+})
 
-// app.get('/return',async(req,resp)=>{
-//     let result=await Returns.find();
-//     return resp.status(200).json(result) 
-// })
+app.get('/return',async(req,resp)=>{
+    let result=await Returns.find();
+    return resp.status(200).json(result) 
+})
 
-// //Salesman API
+//Salesman API
 
-// app.post('/add-salesman',async(req,resp)=>{
-//   let salesMan=new SalesMan(req.body);
-//   let result=await salesMan.save();
-//   return resp.status(200).json(result)
-// })
+app.post('/add-salesman',async(req,resp)=>{
+  let salesMan=new SalesMan(req.body);
+  let result=await salesMan.save();
+  return resp.status(200).json(result)
+})
 
-// app.get('/salesman',async(req,resp)=>{
-//     let result=await SalesMan.find()
-//     return resp.status(200).json(result)
-// })
+app.get('/salesman',async(req,resp)=>{
+    let result=await SalesMan.find()
+    return resp.status(200).json(result)
+})
 
-// app.delete('/salesman/:id',async(req,resp)=>{
-//     let result=await SalesMan.deleteOne({_id:req.params.id})
-//     return resp.status(200).json(result)
+app.delete('/salesman/:id',async(req,resp)=>{
+    let result=await SalesMan.deleteOne({_id:req.params.id})
+    return resp.status(200).json(result)
 
-// })
+})
 
-// //User API
+//User API
 
-// app.post('/add-user',async(req,resp)=>{
-//     let user=new User(req.body);
-//     let result=user.save();
-//     return resp.status(200).json(user);
-// })
+app.post('/add-user',async(req,resp)=>{
+    let user=new User(req.body);
+    let result=user.save();
+    return resp.status(200).json(user);
+})
 app.get('/demo',async(re,resp)=>{
     await resp.send('api working')
 })
@@ -172,9 +172,9 @@ app.get('/user',async(req,resp)=>{
    }
 })
 
-// app.delete('/user/:id',async(req,resp)=>{
-//     let result=await User.deleteOne({_id:req.params.id})
-//     return resp.status(200).json(result)
-// })
+app.delete('/user/:id',async(req,resp)=>{
+    let result=await User.deleteOne({_id:req.params.id})
+    return resp.status(200).json(result)
+})
 
 app.listen(4000,()=>console.log("Run on port 4000"));
